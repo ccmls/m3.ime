a8b7ae65da759d28d482a0483229285a6ff6b43a
ccmls <camila.porfirio@gmail.com>
2020-04-18 22:14:06 +0000

    router

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,27 +3,40 @@ import logo from './assets/logo.svg';
 import './App.css';
 import Header from './components/Header'
 import TopHeader from './components/TopHeader'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <TopHeader></TopHeader>
-      <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          loading
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+            <TopHeader></TopHeader>
+            <Header></Header>
+        </div>
+        <Route exact path="/"> 
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    loading
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </Route>
+        <Route path="/recursos">
+        
+        </Route>
+    </Router>
   );
 }
 
