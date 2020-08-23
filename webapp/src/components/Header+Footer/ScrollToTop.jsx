26677250220c09809d89bf05fa2e4d210fd48847
ccmls <camila.porfirio@gmail.com>
2020-08-23 19:22:54 +0000

    inicio da faxina em header

--- webapp/src/components/Header+Footer/ScrollToTop.jsx
+++ webapp/src/components/Header+Footer/ScrollToTop.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
+// Função para que, ao navegar para uma página dentro do site, ela seja mostrada a partir do topo
 function ScrollToTop({ history }) {
   useEffect(() => {
     const unlisten = history.listen(() => {
--- webapp/src/components/Header+Footer/TopHeader.jsx
+++ webapp/src/components/Header+Footer/TopHeader.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './TopHeader.css';
+// IMAGENS:
 import header from '../../assets/header.png'
 
 function TopHeader() {
     return (
+        // Imagem dos blocos + logotipo na header
         <div className="TopHeader">
             <img src={header} className="imagem-topheader" alt="header" />
         </div>
